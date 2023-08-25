3a017d4633db8f4766b102650013aec7d0663afd
Jamie Robinson <james.robinson@ed.ac.uk>
2023-08-25 17:29:10 +0100

    improve ss_metric scripts

--- ss_metrics/gen_ss_script.sh
+++ ss_metrics/gen_ss_script.sh
@@ -11,15 +11,26 @@ else
 fi
 echo $pop
 
+# pass a db file keyword from the command line
+dbname=$2
+# check if db name is passed via command line
+if [ $# -eq 0 ]
+  then
+    dbname=""
+else
+  dbname=$2
+fi
+echo $dbname
+
 # create a tmp file to store the header inforamtion for cuillin (conda etc)
 echo $PWD
 cp ss_script_header.txt ss_script_header.tmp
 echo "cd $PWD" >> ss_script_header.tmp 
 
 # loop over all db files in this directory
-for f in *.db; do
+for f in *$dbname*.db; do
   echo "db -> $f, pop -> $pop"
-  
+
   # get the new name to save the script 
   arrIN=(${f//.db/ })
   f2=ss_script_${arrIN[0]}_$pop.sh
